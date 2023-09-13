0fe6e5a4fb17986e74c760807503e29e276091c2
so0gom <gomso0vv@gmail.com>
2023-09-13 11:46:29 +0900

    #7 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    초기 기술 :
    - spring-web
    - spring-boot-starter-actuator
    - lombok
    - spring-boot-devtools

---
+++ src/main/java/com/fc/projectp2/FcProjectP2Application.java
@@ -0,0 +1,13 @@
+package com.fc.projectp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcProjectP2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcProjectP2Application.class, args);
+    }
+
+}
